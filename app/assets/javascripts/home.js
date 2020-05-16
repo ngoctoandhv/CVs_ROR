f635f10a52411013427f456f3bca15bdf6ccb8bc
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-05-16 10:17:07 +0700

    add js scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -42,7 +42,7 @@ $(window).on("turbolinks:load", function(){
     }
   });
 
-  $(document).on('click', '.mobile-nav-toggle', function(e) {
+  $(document).on('click', '.mobile-nav-toggle', function() {
     $('body').toggleClass('mobile-nav-active');
     $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times-circle');
   });
@@ -57,6 +57,28 @@ $(window).on("turbolinks:load", function(){
     }
   });
 
+ // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 10;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 200) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
 
 
 });
